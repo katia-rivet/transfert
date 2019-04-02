6bdd5eea500c3a14090ea3b20352998b358f9712
Katia Rivet <47452193+katia-rivet@users.noreply.github.com>
2019-04-02 11:27:21 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
-/*****************************************************
-* 
-* Author :
-* Version :
-* 
-* Description :
-* 
-*/
-
-
-
+require ('upload.php');
+require_once ('connexion.php');
 ?>
-
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -38,31 +28,23 @@
 </head>
 <body>
 
+
+<form action="" method="POST" enctype="multipart/form-data">
+<input type="file" name="fileToUpload" multiple='multiple'>
+<input type="submit">
+</form>
+
+
+
 <?php
-    include('fonction.php');
+uploadFiles();
+
+require ('delete.php');
+
 ?>
 
-    <!-- CONTENT START -->
-
-    <div class="container">
-        <div class="row">
-
-            <form enctype="multipart/form-data" action="" method="post">
-
-                <input type="file" name="fileToUpload">
-                <input type="submit" name="" id="">
-
-            </form>
-        </div>
-    </div>
 
 
-    <?php
-
-    uploadFiles();
-
-
-    ?>
 
     <!-- CONTENT END -->
 
@@ -70,7 +52,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Custom JS -->
     <script src="js/main.js"></script>
 </body>
