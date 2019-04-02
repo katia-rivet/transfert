be7b0e595db1e232cfac29f4517946516d46bd5d
Katia Rivet <47452193+katia-rivet@users.noreply.github.com>
2019-04-02 10:45:03 +0200

    Add files via upload

--- fonction.php
+++ fonction.php
@@ -6,32 +6,45 @@ Upload de fichier
 require_once ('databaseConnection.php');
 
 function uploadFiles(){
-    if(isset($_FILES["fileToUpload"])){
-        $target_dir = './upload/';
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]['name']);
+
+    if (isset($_FILES["fileToUpload"])){
+        
+        
+        $filename_uploaded = explode('.', basename($_FILES["fileToUpload"]["name"])) ;
+        $filename_tmp = basename($_FILES["fileToUpload"]["tmp_name"] .'.'.$filename_uploaded[1]);
+        // echo $filename_tmp; // fichier temporaire
+
+        $target_dir = './upload/' ; 
+        //$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);  
+        $target_file = $target_dir . $filename_tmp;  
+
         move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file);
-    
+
+        $file_nom=$_FILES['fileToUpload']['name'];
+        $tmp_name=$_FILES['fileToUpload']['tmp_name'];
+        
+        $stmt = connectionBase()->prepare("INSERT INTO `upload_files` (`file_nom` , `tmp_name`) VALUES ( :file_nom, :tmp_name);");
+        $stmt->bindParam(':file_nom', $file_nom);
+        $stmt->bindParam(':tmp_name', $tmp_name);
+        $stmt->execute();
+
+
+/*
+faudrait que lorsque je click sur le lien, je reconvertisse le fichier en "vrai" nom
+*/
+
+
+        rename($target_file, $file_nom);
+        var_dump($target_file);
+
+        $file_to_download = ('<a href="upload/' . $filename_tmp . '" download>' . $_FILES["fileToUpload"]["name"] . '</a>');
+        echo($file_to_download);
 
 
 
 
-
-$file_path=$target_file;
-
-$stmt = connectionBase()->prepare("INSERT INTO `upload_files` (`file_path`) VALUES (:file_path);");
-
-
-$stmt->bindParam(':file_path', $file_path);
-
-
-$stmt->execute();
-
-base64_encode($file_path);
-
-echo '<a href="' . $file_path . '" download>' . base64_encode($file_path) . '<a/>';
     }
 }
-
 
 
 // function lien(){
