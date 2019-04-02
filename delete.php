ea8aff3935bff95930d8988a7a8a6d0206991465
Katia Rivet <47452193+katia-rivet@users.noreply.github.com>
2019-04-02 11:46:09 +0200

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php
+require ('fonction.php');
+
+$days = 7;  
+$path = './upload/';  
+
+
+// Open the directory  
+if ($handle = opendir($path))  
+{  
+    // Loop through the directory  
+    while (false !== ($file = readdir($handle)))  
+    {  
+        // Check the file we're doing is actually a file  
+        if (is_file($path.$file))  
+        {  
+            // Check if the file is older than X days old  
+            if (filemtime($path.$file) < ( time() - ( $days * 24 * 60 * 60 ) ) )  
+            {  
+                // Do the deletion  
+                unlink($path.$file);  
+            }  
+        }  
+    }  
+}  
--- fonction.php
+++ fonction.php
@@ -5,39 +5,34 @@ Upload de fichier
 
 require_once ('databaseConnection.php');
 
-function uploadFiles(){
+function uploadFiles(){ 
 
     if (isset($_FILES["fileToUpload"])){
         
-        
-        $filename_uploaded = explode('.', basename($_FILES["fileToUpload"]["name"])) ;
-        $filename_tmp = basename($_FILES["fileToUpload"]["tmp_name"] .'.'.$filename_uploaded[1]);
+        // Séparer à partir '.' des chaines de caractères en segments
+        $filename_uploaded = explode('.', basename($_FILES["fileToUpload"]["name"])) ; 
+        // Concaténation le nom du fichier et l'extension
+        $filename_tmp = basename($_FILES["fileToUpload"]["tmp_name"] .'.'.$filename_uploaded[1]); 
         // echo $filename_tmp; // fichier temporaire
 
-        $target_dir = './upload/' ; 
-        //$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);  
+        $target_dir = './upload/' ;  
         $target_file = $target_dir . $filename_tmp;  
 
+        // Fonction de transfert du dossier tmp dans le dossier cible
         move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file);
 
+        // Récupération des infos du fchier
         $file_nom=$_FILES['fileToUpload']['name'];
         $tmp_name=$_FILES['fileToUpload']['tmp_name'];
         
+        // Enregistrement dans la base SQL
         $stmt = connectionBase()->prepare("INSERT INTO `upload_files` (`file_nom` , `tmp_name`) VALUES ( :file_nom, :tmp_name);");
         $stmt->bindParam(':file_nom', $file_nom);
         $stmt->bindParam(':tmp_name', $tmp_name);
         $stmt->execute();
 
-
-/*
-faudrait que lorsque je click sur le lien, je reconvertisse le fichier en "vrai" nom
-*/
-
-
-        rename($target_file, $file_nom);
-        var_dump($target_file);
-
-        $file_to_download = ('<a href="upload/' . $filename_tmp . '" download>' . $_FILES["fileToUpload"]["name"] . '</a>');
+        // On prépare le téléchargement du fichier et on le reconvertit dans son vrai nom
+        $file_to_download = ('<a href="upload/' . $filename_tmp . '" download="' . $file_nom .'">' . $_FILES["fileToUpload"]["name"] . '</a>');
         echo($file_to_download);
 
 
@@ -47,10 +42,5 @@ faudrait que lorsque je click sur le lien, je reconvertisse le fichier en "vrai"
 }
 
 
-// function lien(){
-//     $target = 'upload/';
-//     $link = 'bootstrap.txt';
-//     link($target, $link);
 
-// }
 ?>
